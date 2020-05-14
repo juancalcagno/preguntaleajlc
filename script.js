011e1969bdd890be8435090ab0c168cd619dccbc
juancalcagno <juancalcagno@gmail.com>
2020-05-14 14:27:23 -0400

    Added aifreim page and ref on scrip.js

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ $(document).ready(function() {
 
         if (DinoInHouse === true) { // if ValidEmail & ValidPassword
             $('.panel').css('display', 'block');
-           // window.location = "https://duckduckgo.com"; // go to home.html
+           // window.location = "https://www.preguntaleajlc.com/aifrein.html"; // go to home.html
         }
         else if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
 
             //window.location = "https://duckduckgo.com"; // go to home.html
-            setTimeout(function(){ window.location.href= 'https://duckduckgo.com';}, 3000);
+            setTimeout(function(){ window.location.href= 'https://www.preguntaleajlc.com/aifrein.html"';}, 3000);
         }
         else {
             $('.error').css('display', 'block'); // show error msg
