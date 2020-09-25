917b07d962c199f41d2d6ed46c0a3912d3627e72
juancalcagno <juancalcagno@gmail.com>
2020-09-24 23:44:02 -0400

    dino imagen repeating FINALLY

--- script.js
+++ script.js
@@ -13,21 +13,19 @@ $(document).ready(function() {
        var DinoInHouse = $('#username').val() === 'dino'
 
         if (DinoInHouse === true) { // if ValidEmail & ValidPassword
-            $('.panel').css('display', 'block');
-           // $('body').css('background-image', 'url(images/NewSevenMagniquesCarlosFace.png)');
-          //  $('body').css({ "background-image": "url(images/NewSevenMagniquesCarlosFace.png)", "background-repeat": "repeat" });
-//           $('body').css({ "background-image" : "url(images/NewSevenMagniques.png)", "background-repeat" : "repeat" });
-//           $('body').css({ "background-image" : "url(images/NewSevenMagniques.png)"});
-           $('body').css('background-image', 'url("images/NewSevenMagniques.png")');
-           $('body').css("background-repeat" , "repeat-y");
+           $('.panel').css('display', 'block');
            
-            
+           $("body").css({ "background-image":"url(images/NewSevenMagniques.png)"
+           ,
+           "background-size" : "initial"
+           ,
+           "background-repeat":"repeat"  });
+//         $("body").css({ "background-image":"url( ' ../images/bodyBack.jpg '           )","background-color":"#f5e7af","background-repeat":"repeat-x"}); 
+
            // window.location = "https://www.preguntaleajlc.com/aifrein.html"; // go to home.html
         }
         else if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
-            $('.valid').css('display', 'block');
-            $('body').css('background-repeat', 'no-repeat');
-            
+            $('.valid').css('display', 'block');            
 
             //window.location = "https://duckduckgo.com"; // go to home.html
             setTimeout(function(){ window.location.href= 'https://preguntaleajlc.com/aifreim.html';}, 3000);
